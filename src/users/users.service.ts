05c9fd3cdd3d130220b3afe79a7fe76bb9bf6eeb
Nurlan Joldibaev <nurlanjoldibaev7@gmail.com>
2023-10-15 20:02:23 +0500

    feat: prisma

---
+++ src/users/users.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  create(email: string, password: string) {
+    return this.prisma.user.create({
+      data: {
+        email,
+        password,
+      },
+    });
+  }
+
+  findAll() {
+    return `This action returns all users`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
