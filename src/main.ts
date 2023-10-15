179dc397549970dd9498911754d3d29843c2e437
Nurlan Joldibaev <nurlanjoldibaev7@gmail.com>
2023-10-15 19:52:42 +0500

    feat: swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { INestApplication } from '@nestjs/common';
+
+function initDocs(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('Open Chat')
+    .setDescription('The Open Chat API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(`docs`, app, document);
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  initDocs(app);
+
   await app.listen(3000);
 }
-bootstrap();
+
+void bootstrap();
